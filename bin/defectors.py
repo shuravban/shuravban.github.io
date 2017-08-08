dac9e593087e1ec5edc5d094256d88bcc94b46ff
yazu <appomsk@gmail.com>
2017-08-08 18:52:20 +0600

    working

--- bin/defectors.py
+++ bin/defectors.py
@@ -2,10 +2,31 @@
 
 import sys, re, fileinput
 import glob, fnmatch, os, os.path, tempfile, shutil
-import time, datetime, sqlite, json
+import time, datetime, sqlite3, json
 
 import tweepy
 
 from boot import api
 
+lst = 'defectors'
 
+# Twitter API - GET friends/ids - 5000 15x15
+c = tweepy.Cursor(api.friends_ids)
+friends = set(c.items())
+
+# -- // --
+c = tweepy.Cursor(api.followers_ids)
+followers = set(c.items())
+
+# After notes - из абстракции только курсор и он сработал - не
+# нужно было вручную отслеживать. А так - прямое отображение
+
+current_rats = friends - followers
+
+# maximum 5000
+# rats = api.list_members('shuravban', 'defectors')
+
+for rat in current_rats:
+    api.add_list_member(owner_screen_name='shuravban', slug=lst, user_id=rat)
+
+# That's all, folks
