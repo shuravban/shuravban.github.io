4c7212ac6fec9155cac4fdf4b4ddbedfa4feabd6
yazu <appomsk@gmail.com>
2017-08-10 08:52:23 +0600

    tweaks

--- bin/boot.py
+++ bin/boot.py
@@ -2,5 +2,6 @@ from twitter_keys import *
 
 from TwitterAPI import TwitterAPI
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token, access_secret)
+api = TwitterAPI(consumer_key, consumer_secret,
+                 access_token_key, access_token_secret)
 
