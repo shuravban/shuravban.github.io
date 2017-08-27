5789c14c834edf0c933aedb913e314aff21ba883
yazu <appomsk@gmail.com>
2017-08-27 11:37:33 +0300

    archived toy scripts

--- archive/boot.py
+++ archive/boot.py
@@ -1,8 +1,7 @@
 from twitter_keys import *
 
-import tweepy
+from TwitterAPI import TwitterAPI
 
-_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-_auth.set_access_token(access_token, access_secret)
+api = TwitterAPI(consumer_key, consumer_secret,
+                 access_token_key, access_token_secret)
 
-api = tweepy.API(_auth)
--- bin/boot.py
+++
@@ -1,7 +0,0 @@
-from twitter_keys import *
-
-from TwitterAPI import TwitterAPI
-
-api = TwitterAPI(consumer_key, consumer_secret,
-                 access_token_key, access_token_secret)
-
